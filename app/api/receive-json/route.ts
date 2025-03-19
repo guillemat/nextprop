eef509af7450ca0f59e6892693f66f2cbcaeb75a
guillemat <guillemat@gmail.com>
2025-03-19 08:54:23 -0400

    {Repair error variable}

--- app/api/receive-json/route.ts
+++ app/api/receive-json/route.ts
@@ -16,37 +16,24 @@ type RequestBody = {
 
 export async function POST(request: NextRequest) {
   try {
-    // Parseamos el cuerpo de la solicitud como JSON
     const body: RequestBody = await request.json();
-      
-    // Convertir el JSON a una cadena (las cookies solo almacenan strings)
     const jsonString = JSON.stringify(body);
-      
-    // Establecer la cookie
     (await cookies()).set('jsonrecibido', jsonString, {
-        //httpOnly: false, // La cookie solo es accesible desde el servidor
-        //secure: process.env.NODE_ENV === 'production', // Solo enviar en HTTPS en producción
-        maxAge: 60 * 60 * 24 * 7, // Caduca en 7 días
-        path: '/', // La cookie estará disponible en toda la aplicación
+        maxAge: 60 * 60 * 24 * 7,
+        path: '/',
     });
-      
-    // Respondemos con un mensaje de éxito
     return NextResponse.json(
       { message: 'JSON recibido correctamente', data: body },
       { status: 200 }
     );
-  } catch (error) {
-    // Si hay un error, respondemos con un código de estado 500
+  } catch (error: unknown) {
     return NextResponse.json(
-      { message: 'Error al procesar el JSON', error: error.message },
+      { message: 'Error al procesar el JSON', error: (error as Error).message },
       { status: 500 }
     );
   }
 }
 
-
-
-// Nueva API para obtener la cookie
 export async function GET() {
   const cookieStore = cookies();
   const jsonData = cookieStore.get('jsonrecibido')?.value;
